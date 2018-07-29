14d72ee60fec3543b10d7a73bd7edf9ec2060a9c
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2018-07-29 02:11:47 -0700

    Changes

--- assets/compiled/script.js
+++ assets/compiled/script.js
@@ -5,6 +5,8 @@ var navShown = false
 $('#nav-toggle').click(function(){
     $('.nav').toggleClass('show');
     $('.opaque-layer').toggleClass('layer-show');
+    $('.bar-2').toggleClass('bar-open');
+    $('.bar-3').toggleClass('bar-open');
     if(navShown == false) {
         navShown = true
     }
@@ -18,11 +20,11 @@ $(document).ready(function() {
        sectionContainer: "section", 
        easing: "ease",
        animationTime: 1000,
-       pagination: true,
+       pagination: false,
        updateURL: false,
        loop: false,
        keyboard: true,
-       responsiveFallback: 720
+       responsiveFallback: 960
     });
 });
 
@@ -33,3 +35,6 @@ $('.opaque-layer').click(function() {
         navShown = false
     }
 });
+
+
+$("#home").on({mouseenter:function(){$(".nav").css("background-color","#2ecc71")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#about").on({mouseenter:function(){$(".nav").css("background-color","#DC3023")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#projects").on({mouseenter:function(){$(".nav").css("background-color","#1abc9c")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#facilities").on({mouseenter:function(){$(".nav").css("background-color","#f1c40f")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#academics").on({mouseenter:function(){$(".nav").css("background-color","#32ff7e")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#team").on({mouseenter:function(){$(".nav").css("background-color","#7158e2")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}}),$("#contact").on({mouseenter:function(){$(".nav").css("background-color","#ffb8b8")},mouseleave:function(){$(".nav").css("background-color","#ffffff")}});
